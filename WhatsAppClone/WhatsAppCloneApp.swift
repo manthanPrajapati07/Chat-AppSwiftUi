ce5dc47fcd2cfdf5fe4cc11a2906909c4cf1fe12
Manthan Prajapati <manthanprajapati189@gmail.com>
2025-05-13 22:37:11 +0530

    commit

---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Manthan on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
