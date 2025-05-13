ba042115a60bf3c2ca348d9fc89e3bcaa8934118
Manthan Prajapati <manthanprajapati189@gmail.com>
2025-05-14 00:28:47 +0530

    commit

--- WhatsAppClone/ContentView.swift
+++ WhatsAppClone/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isActive : Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isActive{
+                LoginView()
+            }else{
+                SplashView()
+            }
         }
-        .padding()
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0){
+                isActive.toggle()
+            }
+        }
     }
 }
 
---
+++ WhatsAppClone/SplashView.swift
@@ -0,0 +1,50 @@
+//
+//  SplashView.swift
+//  WhatsAppClone
+//
+//  Created by Manthan on 13/05/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            HStack{
+                VStack{
+                    Spacer()
+                    
+                    splashIcn
+                        .frame(width: geometry.size.width * 0.3)
+                    
+                    Spacer()
+                    
+                    titleView
+                        .padding(.bottom,30)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+    
+    
+    var splashIcn: some View {
+        Image("Splash_icn")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+    }
+    
+    
+    var titleView: some View {
+        VStack(spacing: 4) {
+            Text("form")
+                .font(.system(size: 20, weight: .regular))
+            Text("FACEBOOK")
+                .font(.system(size: 22, weight: .bold))
+        }
+    }
+}
+
+#Preview {
+    SplashView()
+}
