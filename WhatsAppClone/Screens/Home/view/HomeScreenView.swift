44565398bb5748d54b3e79483270834df2bbae45
Manthan Prajapati <manthanprajapati189@gmail.com>
2025-05-25 02:40:23 +0530

    commit

--- WhatsAppClone/Screens/Home/view/HomeScreenView.swift
+++ WhatsAppClone/Screens/Home/view/HomeScreenView.swift
@@ -8,9 +8,75 @@
 import SwiftUI
 
 struct HomeScreenView: View {
+    
+    @State var homeVM = HomeViewModel.shared
+    @State var selectedValue : String = "Explore"
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                customNavBarView
+                customSegment(with: homeVM.segmentArrey, selected: selectedValue)
+                    .padding(.horizontal, 10)
+                Spacer()
+            }
+        }
     }
+    
+    var customNavBarView: some View{
+        ZStack{
+            Text("Chats")
+                .font(.system(size: 20, weight: .medium))
+            HStack{
+                Spacer()
+                Spacer()
+            }
+        }
+        .frame(height: 30)
+        .background(Color(.systemGray6))
+    }
+    
+    private func customSegment(with tabs:[String], selected: String) -> some View{
+        GeometryReader{ geometry in
+            
+            let geometryWidth = geometry.size.width / CGFloat(tabs.count)
+            
+            ZStack(alignment: .leading){
+                HStack{
+                    Rectangle()
+                        .background(Color.black)
+                        .frame(width: geometryWidth - 14)
+                        .cornerRadius(geometry.size.height / 2)
+                        .offset(x: CGFloat(tabs.firstIndex(of: selected) ?? 0) * geometryWidth)
+                        .animation(.easeInOut(duration: 0.4), value: selected)
+                }
+                .padding([.leading, .top, .bottom], 7)
+                
+                HStack{
+                    ForEach(tabs, id: \.self) { segment in
+                        Button {
+                            selectedValue = segment
+                        } label: {
+                            HStack{
+                                Spacer()
+                                Text(segment)
+                                    .multilineTextAlignment(.center)
+                                    .font(.system(size: 20, weight: .medium))
+                                    .foregroundStyle(selectedValue == segment ? Color.white : Color.black)
+                                Spacer()
+                            }
+                        }
+                        .scaleEffect(1)
+                    }
+                }
+            }
+            .frame(height: 50)
+            .background(Color(uiColor: .systemGray6))
+            .cornerRadius(geometry.size.height / 2)
+        }
+    }
+    
 }
 
 #Preview {
---
+++ WhatsAppClone/Screens/Home/viewModel/HomeViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewModel.swift
+//  WhatsAppClone
+//
+//  Created by Manthan on 24/05/25.
+//
+
+import Foundation
+import SwiftUI
+
+final class HomeViewModel{
+    
+    static let shared = HomeViewModel()
+    private init(){}
+    
+    let segmentArrey = ["Explore", "Friends"]
+}
+
+
